76f769269313933c764166beae37c5a66160863b
Brianna Scott <44863228+simplybrix@users.noreply.github.com>
2021-10-06 20:54:41 -0400

    Add files via upload

---
+++ BScott_TipCalculator.py
@@ -0,0 +1,18 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+#HINT 1: https://www.google.com/search?q=how+to+round+number+to+2+decimal+places+python&oq=how+to+round+number+to+2+decimal
+#HINT 2: https://www.kite.com/python/answers/how-to-limit-a-float-to-two-decimal-places-in-python
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people are splitting the bill? "))
+
+tip_percent = tip / 100
+total_tip = bill * tip_percent
+total_bill = bill + total_tip
+bill_per_person = total_bill / people
+final_price = round(bill_per_person, 2)
+
+print(f"Each person should pay: ${final_price}")
