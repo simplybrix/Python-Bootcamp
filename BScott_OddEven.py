81d2a08081047b17923b996081cadc93a93d9dd7
Brianna M. Scott <Brianna.m.scott@hotmail.com>
2021-10-10 05:18:01 -0400

    Create BScott_OddEven.py

---
+++ BScott_OddEven.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if number % 2 == 0:
+  print("The number inputed is a even number.")
+else:
+  print("The number inputed is an odd number.")
